4daf81e922ad53bef0a0d79a740e31e368285602
vpavelko <vpavelko@softjourn.com>
2023-09-12 09:12:47 +0300

    add data anotations

--- StockMarket/DTOs/LotItemRequest.cs
+++ StockMarket/DTOs/LotItemRequest.cs
@@ -1,8 +1,14 @@
-﻿namespace StockMarket.DTOs
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockMarket.DTOs
 {
     public class LotItemRequest
     {
+        [Range(1, int.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
+        [DefaultValue(1)]
         public int ShareNumber { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
         public decimal SharePrice { get; set; }
         public DateTime Date { get; set; }
     }
